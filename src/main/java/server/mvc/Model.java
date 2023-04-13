cf273068ffd98c5d50f5bd2d973e2d52dbdd7309
mildshield14 <80240232+mildshield14@users.noreply.github.com>
2023-04-13 01:31:55 -0400

    Update Model.java

--- src/main/java/server/mvc/Model.java
+++ src/main/java/server/mvc/Model.java
@@ -1,20 +1,22 @@
 package server.mvc;
 
-import javafx.application.Application;
-import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 import server.Client;
+import server.models.Course;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Model{
 
 private TextField value1;
+
+private ArrayList<Course> entries;
 private TextField value2;
 private TextField value3;
 private TextField value4;
-        private TextField value5;
+
+private String code;
 private String errorMessage;
 private String infoError;
 public Model(){
@@ -22,24 +24,38 @@ public Model(){
         value2 = new TextField();
         value3 = new TextField();
         value4 = new TextField();
-        value5 = new TextField();
+        code= "";
         }
 
 public String getValue1() {
         return value1.getText();
         }
 
-public void setValue1(String value1) {
+        public String getCode() {
+                return code;
+        }
+
+        public void setCode(String code) {
+                this.code = code;
+        }
+
+        public void setValue1(String value1) {
         this.value1.setText(value1);
         }
 
-public String getValue2() {
+        public void setEntries(ArrayList<Course> entries) {
+                this.entries = entries;
+        }
+
+        public ArrayList<Course> getEntries() {
+                return entries;
+        }
+
+        public String getValue2() {
         return value2.getText();
         }
 
-        public void charger(){
 
-        }
 
 public void setValue2(String value2) {
         this.value2.setText(value2);
@@ -61,36 +77,36 @@ public void setValue4(String value4) {
         this.value4.setText(value4);
         }
 
-        public  void setValue5(String value5) {
-                this.value5.setText(value5);
-        }
-public String getValue5() {
-                return value5.getText();
-        }
-public void updateData(String values1, String values2, String values3, String values4, String values5) {
+
+public void updateData(String values1, String values2, String values3, String values4, String code) {
         setValue1(values1);
         setValue2(values2);
         setValue3(values3);
         setValue4(values4);
-        setValue5(values5);
+        setCode(code);
         }
 
-        public void setError(String errorMessage) {
+public void setError(String errorMessage) {
                 this.errorMessage = errorMessage;
         }
-
-        public String getError() {
+public String getError() {
                 return this.errorMessage;
         }
-
-        public String getInfoError(){
+public String getInfoError(){
         return this.infoError;
         }
-
-        public void setInfoError(String infoError){
+public void setInfoError(String infoError){
         this.infoError=infoError;
         }
+        
+public void charger(String com) throws IOException, ClassNotFoundException {
 
+
+        ArrayList<Course> en;
+        en=Client.charger1(com);
+        setEntries(en);
+
+}
 public void envoyer() {
         boolean b = true;
 
@@ -98,21 +114,21 @@ public void envoyer() {
         String nom= String.valueOf((getValue2()));
         String email= String.valueOf((getValue3()));
         String matricule= String.valueOf((getValue4()));
-        String code= String.valueOf((getValue5()));
+
         String k=prenom;
         setInfoError("");
         if (!(k .equals("") || nom.equals("") || email.equals("") || matricule.equals("")|| code.equals(""))){
-        ArrayList<String> entries = new ArrayList<>();
-                setError("");
-        entries.add(prenom);
-        entries.add(nom);
-        entries.add(email);
-        entries.add(matricule);
-        entries.add(code);
-
-        Client.startGUI(entries);
-
-        setInfoError(Client.GUIMessage());
+                ArrayList<String> entries = new ArrayList<>();
+                        setError("");
+                entries.add(prenom);
+                entries.add(nom);
+                entries.add(email);
+                entries.add(matricule);
+                entries.add(getCode());
+        
+                Client.startGUI(entries);
+        
+                setInfoError(Client.GUIMessage());
 
         }
 
@@ -121,7 +137,8 @@ public void envoyer() {
           setError("Text boxes cannot be empty");
 
         }
-        }
+        
+    }
 
 
 
