92ed5d2c053d8619d6ecef61b2068691a201a56e
marionnee <114243727+marionnee@users.noreply.github.com>
2023-04-16 18:44:12 -0400

    Javadoc+ debut multithreading

--- src/main/java/server/ClientLauncher.java
+++ src/main/java/server/ClientLauncher.java
@@ -3,34 +3,58 @@ package server;
 import java.io.IOException;
 import java.net.Socket;
 
-public class ClientLauncher {
+/**
+ * Lance le client
+ */
+public class ClientLauncher extends Thread {
     protected static Socket socket;
 
-    public static final Integer port=1337;
+    /**
+     * Le port utilisé
+     */
+    public static final Integer port = 1337;
 
-    public static final String address="localhost";
+    /**
+     *
+     */
+    public static final String address = "localhost";
 
+    /**
+     * Permet de déconnecter le client
+     * @throws IOException Si le port ou l'adresse sont incorrects
+     */
+    public static void closeSocket() throws IOException {
+        if (socket != null && !socket.isClosed()) {
+            socket.close();
+        }
+    }
 
-public static void closeSocket() throws IOException {
-    if (socket != null && !socket.isClosed()) {
-        socket.close();}
-}
+    /**
+     * Récupère le socket
+     * @return Retourne le socket
+     */
     public static Socket getSocket() {
         return socket;
     }
+
+    /**
+     * Lance le client
+     * @param args
+     */
+
     public static void main(String[] args) {
-      Client client;
 
+        //Client client;
 
-        try {
-            client = new Client(port);
-            System.out.println("Client is running...");
-            client.prepTransfer();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+            try {
+                Client thread1=new Client(port);
+                //client = new Client(port);
+                System.out.println("Client is running...");
+                thread1.prepTransfer();
+                thread1.start();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
     }
-    public static void setConnection() throws IOException {
-        socket = new Socket(address, (port));
-    }
-}
+
