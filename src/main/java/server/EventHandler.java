3f8c9869fbe81f61ea4c5b22fb2ba9fe5e23b41a
marionnee <114243727+marionnee@users.noreply.github.com>
2023-04-17 13:01:48 -0400

    Update EventHandler.java

--- src/main/java/server/EventHandler.java
+++ src/main/java/server/EventHandler.java
@@ -1,12 +1,6 @@
 package server;
-import java.io.IOException;
 
 @FunctionalInterface
-/**
- * Réagit aux requêtes
- * @param cmd commande associée à la saisie de l'utilisateur
- * @param arg saisie de l'utilisateur
- */
 public interface EventHandler {
-    void handle(String cmd, String arg) throws IOException, ClassNotFoundException;
+    void handle(String cmd, String arg);
 }
