ebad58aea1242310e6657208b70c2c8c7721ba34
marionnee <114243727+marionnee@users.noreply.github.com>
2023-04-17 15:50:08 -0400

    javadoc

--- src/main/java/server/mvc/Main.java
+++ src/main/java/server/mvc/Main.java
@@ -2,7 +2,14 @@ package server.mvc;
 
 import javafx.application.Application;
 
+/**
+ * Permet de lancer le GUI
+ */
 public class Main {
+    /**
+     * Lance l'application
+     * @param args
+     */
     public static void main(String[] args) {
         Application.launch(GUI.class, args);
     }
